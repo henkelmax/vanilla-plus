5e539822f64f64629f96ee99c9f2277968d8c527
Max Henkel <max@maxhenkel.de>
2022-07-27 19:53:16 +0200

    Update to 1.19.1

--- src/main/java/de/maxhenkel/vanillaplus/mixin/BadOmenMobEffectMixin.java
+++ src/main/java/de/maxhenkel/vanillaplus/mixin/BadOmenMobEffectMixin.java
@@ -14,7 +14,10 @@ public class BadOmenMobEffectMixin {
 
     @Redirect(method = "applyEffectTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/raid/Raids;createOrExtendRaid(Lnet/minecraft/server/level/ServerPlayer;)Lnet/minecraft/world/entity/raid/Raid;"))
     private Raid registerGoals(Raids raids, ServerPlayer player) {
-        if (VanillaPlus.SERVER_CONFIG.controlledRaids.get() && ((VanillaPlusAbilities) player.getAbilities()).canTriggerRaids()) {
+        if (!VanillaPlus.SERVER_CONFIG.controlledRaids.get()) {
+            return raids.createOrExtendRaid(player);
+        }
+        if (((VanillaPlusAbilities) player.getAbilities()).canTriggerRaids()) {
             return raids.createOrExtendRaid(player);
         } else {
             return null;
